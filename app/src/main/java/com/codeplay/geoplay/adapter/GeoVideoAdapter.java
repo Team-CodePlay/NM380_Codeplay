186bf29963bebe218782f89e98e4066337501ef5
Daniel Lobo <danlobo1999@gmail.com>
2020-08-02 01:13:47 +0530

    added: GeoVideoViewHolder Class to GeoVideoAdapter

--- app/src/main/java/com/codeplay/geoplay/adapter/GeoVideoAdapter.java
+++ app/src/main/java/com/codeplay/geoplay/adapter/GeoVideoAdapter.java
@@ -1,9 +1,13 @@
 package com.codeplay.geoplay.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,11 +31,24 @@ public class GeoVideoAdapter extends RecyclerView.Adapter<GeoVideoAdapter.GeoVid
 		this.dataset = dataset;
 	}
 
+	private static final int detailedVideoCard = 1;
+	private static final int largeThumbnailVideoCard = 0;
+	public boolean toggleCard = false;
+
+	@Override
+	public int getItemViewType(int position) {
+		return toggleCard? largeThumbnailVideoCard: detailedVideoCard;
+	}
+
 	@NonNull
 	@Override
 	public GeoVideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_video_detailed, parent, false);
-		return new GeoVideoViewHolder(view);
+		if (viewType == 1) {
+			return new GeoVideoViewHolder(LayoutInflater.from(parent.getContext())
+					.inflate(R.layout.row_video_detailed, parent, false));
+		}
+		return new GeoVideoViewHolder(LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.row_video_large_thumbnail, parent, false));
 	}
 
 	@Override
@@ -44,9 +61,26 @@ public class GeoVideoAdapter extends RecyclerView.Adapter<GeoVideoAdapter.GeoVid
 		return dataset.size();
 	}
 
-	public class GeoVideoViewHolder extends RecyclerView.ViewHolder{
+	public static class GeoVideoViewHolder extends RecyclerView.ViewHolder{
+		public TextView vidTitle;
+		public TextView date;
+		public TextView duration;
+		public TextView size;
+		public TextView location;
+		public ImageView snapshot;
+		public TextView optionsIconText;
+		public ImageView uploaded;
+
 		public GeoVideoViewHolder(@NonNull View itemView) {
 			super(itemView);
+			vidTitle = itemView.findViewById(R.id.videoTitle);
+			snapshot = itemView.findViewById(R.id.snapshot);
+			optionsIconText = itemView.findViewById(R.id.options);
+			date = itemView.findViewById(R.id.date);
+			duration = itemView.findViewById(R.id.duration);
+			size = itemView.findViewById(R.id.size);
+			location = itemView.findViewById(R.id.location);
+			uploaded = itemView.findViewById(R.id.upload_icon);
 		}
 	}
 
