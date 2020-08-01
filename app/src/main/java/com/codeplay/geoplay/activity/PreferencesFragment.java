62f671e7c9650deab5073d5747b5a0c3b29f3d28
Jaineel-Mamtora <jmamtora99@gmail.com>
2020-08-01 17:06:09 +0530

    added: Preferences Fragment

--- app/src/main/java/com/codeplay/geoplay/activity/PreferencesFragment.java
+++ app/src/main/java/com/codeplay/geoplay/activity/PreferencesFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.codeplay.geoplay.R;
+
 public class PreferencesFragment extends PreferenceFragmentCompat {
-	@Override
-	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-		// TODO: 01-08-2020 add preferences
-	}
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_preferences, rootKey);
+    }
 }
