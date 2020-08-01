67ed6b8eb3e0ccdb4eb47e5688e71a939b73f5a1
Sahil Nirkhe <sahilnirkhe@outlook.com>
2020-08-01 16:44:28 +0530

    added: maps in record and playback activity

--- app/src/main/java/com/codeplay/geoplay/activity/PlaybackActivity.java
+++ app/src/main/java/com/codeplay/geoplay/activity/PlaybackActivity.java
@@ -5,15 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.codeplay.geoplay.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * For map and video playback, info and sharing.
  */
-public class PlaybackActivity extends AppCompatActivity {
+public class PlaybackActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+	GoogleMap mMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playback);
+
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		mMap = googleMap;
+
+		// Add a marker in Sydney and move the camera
+		LatLng sydney = new LatLng(-34, 151);
+		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 	}
 }
