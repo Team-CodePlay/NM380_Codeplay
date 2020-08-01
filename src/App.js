f7e8477bb25628cc0cdb0e9066aa5ef1bf9aad64
HOD101s <manasbass.99@gmail.com>
2020-08-01 17:05:37 +0530

    Added Basic Map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MultiMap from './components/MultiMap/MultiMap';
 
 function App() {
   return (
     <div className="App">
-      <h1>Codeplay : GeoTagging Videos</h1>
+      <MultiMap />
     </div>
   );
 }
---
+++ src/components/MultiMap/MultiMap.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import {
+    withGoogleMap,
+    GoogleMap,
+    withScriptjs,
+} from "react-google-maps";
+
+const MultiMap = () => {
+
+    const MapWithPaths = () => {
+        return (
+            <GoogleMap
+                defaultZoom={15}
+                defaultCenter={{ lat: 19.23, lng: 72.85 }}
+            ></GoogleMap>
+        );
+    };
+
+    const MyMap = withScriptjs(withGoogleMap(MapWithPaths));
+
+    return (
+        <MyMap
+            googleMapURL={"https://maps.googleapis.com/maps/api/js?key="+process.env.REACT_APP_GOOGLE_MAPS_OLD_API_KEY+"&v=3.exp&libraries=geometry,drawing,places"}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `60vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+    );
+};
+
+
+export default MultiMap;
