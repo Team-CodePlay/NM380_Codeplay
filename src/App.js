de560c4fb16a946538d9fa59e6bfeb14e6fd8072
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-08-01 18:07:51 +0530

    Add Header to the root App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
-import MultiMap from './components/MultiMap/MultiMap';
+import React from "react";
+import "./App.css";
+import MultiMap from "./components/MultiMap/MultiMap";
+import Header from "./components/MultiMap/layouts/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MultiMap />
     </div>
   );
