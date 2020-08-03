b5331c192e175ec77ff7e0013d389c762a6060cd
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-08-03 11:24:58 +0530

    Change header to "Admin Panel"

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Navbar, Nav,NavLink } from 'react-bootstrap';
+import { Navbar, Nav, NavLink } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <div>
-      
+
       <Navbar bg="primary" variant="dark" expand="lg" className="mx-auto">
         <Navbar.Brand expand="lg" href="/">
           <img src="/favicon.ico" width="30" alt="Product Logo" />
@@ -14,7 +14,7 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <NavLink href="/"><i class="fas fa-home"></i> Home</NavLink>
-            <NavLink href="https://console.firebase.google.com/u/0/project/geoplay-codeplay/overview" target="_blank"><span style={{marginBottom:"5px"}} class="iconify" data-icon="simple-icons:firebase" data-inline="false"></span> Firebase Console</NavLink>
+            <NavLink href="https://console.firebase.google.com/u/0/project/geoplay-codeplay/overview" target="_blank"><span style={{ marginBottom: "5px" }} class="iconify" data-icon="mdi:monitor-dashboard" data-inline="false"></span> Admin Panel</NavLink>
             {/* <NavLink href="/dashboard">Dashboard</NavLink> */}
           </Nav>
         </Navbar.Collapse>
